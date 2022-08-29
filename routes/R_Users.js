2bc6dfe7cff436b9920a5383f886bcbc7719a749
hoadang <dangthaihoa1105@gmail.com>
2022-08-29 16:00:52 +0700

    update user form

--- routes/R_Users.js
+++ routes/R_Users.js
@@ -4,6 +4,15 @@ const router = express.Router()
 router.get('/index', (req, res) => {
     var path = 'users/main'
     res.render('index', {path})
-})
+});
 
+router.get('/add', (req, res) => {
+    var path = 'users/form'
+    res.render('index', {path})
+});
+
+router.get('/edit/:id', (req, res) => {
+    var path = 'users/form'
+    res.render('index',     {path})
+});
 module.exports = router;
